ce0fbf3c562ea96b039320ac799649776bf176c4
MoniseurGreatGuy <mini-kalle@hotmail.com>
2017-03-14 19:56:54 +0100

    tilføj alkoholiker klasse

---
+++ app/src/main/java/com/example/sebastian/mindrukapp/Alkoholiker.java
@@ -0,0 +1,26 @@
+package com.example.sebastian.mindrukapp;
+
+/**
+ * Created by Sebastian on 14-Mar-17.
+ */
+
+public class Alkoholiker {
+    private String name;
+    private int genstande;
+    private int sips;
+
+    public Alkoholiker(String name) {
+        this.name = name;
+    }
+
+    public void drik(int sip){
+        if((sips+sip) >= 10)
+        {
+
+        }
+    }
+
+    public int getGenstande() {
+        return genstande;
+    }
+}
--- app/src/main/java/com/example/sebastian/mindrukapp/StartgameActivity.java
+++ app/src/main/java/com/example/sebastian/mindrukapp/StartgameActivity.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StartgameActivity extends AppCompatActivity {
+
+    private List<Alkoholiker> alkoholikerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startgame);
+
+        alkoholikerList = new ArrayList<>();
+
     }
 
 }
