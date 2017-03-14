67be201166727d4acec1ad52bc909b7a6029ef15
MoniseurGreatGuy <mini-kalle@hotmail.com>
2017-03-14 18:47:41 +0100

    added activity

--- app/src/main/java/com/example/sebastian/mindrukapp/StartgameActivity.java
+++ app/src/main/java/com/example/sebastian/mindrukapp/StartgameActivity.java
@@ -1,17 +1,15 @@
 package com.example.sebastian.mindrukapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class StartgameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void onMainClick(View view){
         setContentView(R.layout.activity_startgame);
     }
+
 }
